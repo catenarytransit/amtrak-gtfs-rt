c22231abb1f8c7a555f2aac06d3f4fd9c9df01f7
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-02-03 04:26:49 -0800

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -446,16 +446,14 @@ fn feature_to_gtfs_unified(
     };
 
     let alert = match &train_num {
-        Some(train_num) => {
-            match asm_lookup_table {
-                Some(asm_lookup_table) => {
-                    match asm_lookup_table.get(&(origin_local_time.date_naive(), train_num.clone())) {
-                        Some(alerts) => asm_alert_to_gtfs_rt(informed_entity, alerts),
-                        None => None,
-                    }
+        Some(train_num) => match asm_lookup_table {
+            Some(asm_lookup_table) => {
+                match asm_lookup_table.get(&(origin_local_time.date_naive(), train_num.clone())) {
+                    Some(alerts) => asm_alert_to_gtfs_rt(informed_entity, alerts),
+                    None => None,
                 }
-                None => None,
             }
+            None => None,
         },
         None => None,
     };
